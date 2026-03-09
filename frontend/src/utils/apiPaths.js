e838a6a282655852445c6264d74e4ec004680636
TechieBahdie <adeolaakinbola5@gmail.com>
2026-03-09 04:02:35 +0100

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://expensetracker-fullstackmern-backend.onrender.com/api/v1";
 
 // utils/apiPaths.js
 export const API_PATHS = {
